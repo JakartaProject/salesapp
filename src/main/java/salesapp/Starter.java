ec209b36bb8e2939953d56993fe3826bfd0352b3
meikarta <meikarta@10.128.171.88>
2018-08-10 13:36:04 +0700

    数据库orm层

--- src/main/java/salesapp/Starter.java
+++ src/main/java/salesapp/Starter.java
@@ -1,11 +1,62 @@
 package salesapp;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Starter {
 	public static void main(String[] args) {
 		SpringApplication.run(Starter.class, args);
+	}
+
+	@Configuration
+	@MapperScan(basePackages = "salesapp.mybatis.mapper", sqlSessionTemplateRef = "salesSqlSessionTemplate")
+	public static class DataSourceSalesConfig {
+		@Bean(name = "salesDataSource")
+		@ConfigurationProperties(prefix = "spring.datasource.sales")
+		@Primary
+		public DataSource testDataSource() {
+			return DataSourceBuilder.create().build();
+		}
+
+		@Bean(name = "salesSqlSessionFactory")
+		@Primary
+		public SqlSessionFactory testSqlSessionFactory(@Qualifier("salesDataSource") DataSource dataSource)
+				throws Exception {
+			SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+			bean.setDataSource(dataSource);
+//			bean.setMapperLocations(
+//					new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/sales/*.xml"));
+			return bean.getObject();
+		}
+
+		@Bean(name = "salesTransactionManager")
+		@Primary
+		public DataSourceTransactionManager testTransactionManager(
+				@Qualifier("salesDataSource") DataSource dataSource) {
+			return new DataSourceTransactionManager(dataSource);
+		}
+
+		@Bean(name = "salesSqlSessionTemplate")
+		@Primary
+		public SqlSessionTemplate testSqlSessionTemplate(
+				@Qualifier("salesSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+			return new SqlSessionTemplate(sqlSessionFactory);
+		}
 	}
 }
--- src/main/java/salesapp/TestController.java
+++ src/main/java/salesapp/TestController.java
@@ -1,13 +1,23 @@
 package salesapp;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import salesapp.mybatis.entity.User;
+import salesapp.mybatis.mapper.UserMapper;
+
 @RestController
 public class TestController {
+	@Resource
+	private UserMapper userMapper;
+	
 	@RequestMapping(value = "test", method = RequestMethod.GET)
 	public String test() {
+		User u = userMapper.selectByPrimaryKey(123L);
+		System.out.println(u);
 		return "hello world!";
 	}
 }
