955dfca8c617f6480e2157f31955145c49b6e099
JakartaProject <42025684+JakartaProject@users.noreply.github.com>
2018-08-29 14:55:11 +0700

    将需求记录到注释

--- src/main/java/salesapp/Starter.java
+++ src/main/java/salesapp/Starter.java
@@ -22,8 +22,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Starter {
 	// 新需求
 	// 1、维持我的消息
-	// 2.增加查房条件
-	// 3.客户可以在系统保持多份备份，每个销售都可以增加一个备份，避免销售之间查看各自跟进信息，导致矛盾
+	// 2.增加查房条件，按楼层查
+	// 3.通过精确查询到客户的信息后，在线下联系对应销售未果的情况下，其他销售可以“代跟进”，但是不能看到客户的跟进信息列表
+	// 4.销售的客户不再跟进，先进入经理的客户池，在经理的客户池若干天不分配，则进入副总客户池
+	// 5.增加管理员账号，查看副总信息列表，增删副总账号
 	public static void main(String[] args) {
 		SpringApplication.run(Starter.class, args);
 	}
